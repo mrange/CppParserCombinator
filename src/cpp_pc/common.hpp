c9f662855ab2188c30ef05a3a0ea348e88c8353f
mrange <marten_range@hotmail.com>
2015-06-29 19:27:18 +0200

    Updated encoding

--- src/cpp_pc/common.hpp
+++ src/cpp_pc/common.hpp
@@ -1,5 +1,5 @@
 // ----------------------------------------------------------------------------
-// Copyright 2015 Mårten Rånge
+// Copyright 2015 MÃ¥rten RÃ¥nge
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
